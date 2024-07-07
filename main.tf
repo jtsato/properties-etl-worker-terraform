4d8366967a74b4f4654d7cdf40b35536cfec9e98
Jorge Takeshi Sato <jorge.takeshi.sato@gmail.com>
2024-07-07 04:50:26 -0300

    fix(terraform): change port to 3000

--- main.tf
+++ main.tf
@@ -95,3 +95,53 @@ resource "google_cloud_run_v2_service" "default" {
   }
   
 }
+
+data "google_project" "project" {
+  project_id = var.project_id
+}
+
+resource "google_service_account" "default_service_account" {
+  account_id   = var.service_name
+  display_name = var.service_name
+  project      = data.google_project.project.project_id
+}
+
+resource "google_service_account_iam_binding" "act_as_iam" {
+  service_account_id = google_service_account.default_service_account.name
+  role               = "roles/iam.serviceAccountUser"
+  members = [
+    "serviceAccount:${google_service_account.default_service_account.email}",
+  ]
+}
+
+resource "google_service_account_iam_member" "iam_member" {
+  service_account_id = google_service_account.default_service_account.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${google_service_account.default_service_account.email}"
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_policy" "noauth" {
+  project  = google_cloud_run_v2_service.default.project
+  location = google_cloud_run_v2_service.default.location
+  name     = google_cloud_run_v2_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+# gsutil mb -p duckhome-firebase -c STANDARD -l southamerica-east1 gs://duckhome-etl-terraform-state
+terraform {
+  backend "gcs" {
+    bucket = "duckhome-etl-terraform-state"
+    prefix = "terraform/state"
+  }
+}
