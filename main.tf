c9f50d10116f83c1b92bd174fefb5266dc327b40
Jorge Takeshi Sato <jorge.takeshi.sato@gmail.com>
2024-07-06 22:12:05 -0300

    Initial Commit

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "google_cloud_run_service" "default" {
         image = var.image_url
 
         ports {
-          container_port = 80
+          container_port = 8000
         }
         env {
           name  = "TZ"
