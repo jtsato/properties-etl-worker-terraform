3c5ea79713ca510ae83b23978d24aca4265185a2
Jorge Takeshi Sato <jorge.takeshi.sato@gmail.com>
2024-07-08 00:41:37 -0300

    refactor(infra): just run the pipeline

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "google_cloud_run_v2_service" "default" {
 
       resources {
         limits = {
-          memory = "512Mi"
+          memory = "1024Mi"
           cpu   = "2"
         }
       }
