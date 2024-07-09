7f5aa3a92c62544478028ff4f36caeee4e874487
Jorge Takeshi Sato <jorge.takeshi.sato@gmail.com>
2024-07-09 01:55:48 -0300

    refactor(app): use google storage bucket

--- main.tf
+++ main.tf
@@ -154,6 +154,11 @@ resource "google_storage_bucket" "storage_bucket" {
   project                     = var.project_id
   force_destroy               = true
   uniform_bucket_level_access = true
+
+  lifecycle {
+    prevent_destroy = false
+  }  
+
 }
 
 data "google_iam_policy" "storage_bucket_policy" {
