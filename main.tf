89fe614890db5658cdbd6617f4930283b287d8f2
Jorge Takeshi Sato <jorge.takeshi.sato@gmail.com>
2024-07-09 01:37:47 -0300

    refactor(terraform): avoid gs bucket removal

--- main.tf
+++ main.tf
@@ -152,8 +152,11 @@ resource "google_storage_bucket" "storage_bucket" {
   name                        = "${var.bucket_name}"
   location                    = google_cloud_run_v2_service.default.location
   project                     = var.project_id
-  force_destroy               = false
   uniform_bucket_level_access = true
+
+  lifecycle {
+    prevent_destroy = true
+  }  
 }
 
 data "google_iam_policy" "storage_bucket_policy" {
