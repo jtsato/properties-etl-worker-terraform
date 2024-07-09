4cb5c826a99df081961d5c024e185a61223abc1b
Jorge Takeshi Sato <jorge.takeshi.sato@gmail.com>
2024-07-09 17:42:14 -0300

    refactor(infra): set prevent to destroy = true

--- main.tf
+++ main.tf
@@ -2,8 +2,11 @@ resource "google_storage_bucket" "storage_bucket" {
   name                        = var.bucket_name
   location                    = var.cloud_region
   project                     = var.project_id
-  force_destroy               = true
   uniform_bucket_level_access = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 data "google_iam_policy" "storage_bucket_policy" {
